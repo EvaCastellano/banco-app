453867d71081318d78b97600ec6ebf5ce4f4e4d5
EvaCastellano <evacastellano60@gmail.com>
2024-02-21 12:53:24 +0100

    Implementando login basado en datos locales

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -8,70 +8,77 @@ import Movements from './Movements/Movements'
 import Balance from './Balance/Balance'
 
 function App() {
-  const [account, setAccount] = useState(accounts[0]) // useState({})
-  const { movements=[], owner:user='' } = account
-  /* 
-   TAREAS
-   2. Hacer los movimientos.
-      Recibe una propiedad que es el array de los movimientos.
-      Muestra una lista de movimientos que son un componente llamado Movement que recibe una propiedad que es el movimiento.
+  const [account, setAccount] = useState({}) // useState(accounts[0])  -> para tener la pantalla de Juan fija
+  const { movements = [], owner: user = '' } = account
 
-   3. Hacer el componente Login -> usar UseRef como ya hicimos para hacer el login.
-  */
+  const handleLogin = (user, pin) => {
+    //
+    const currentAccount = accounts.find(
+      (account) => account.username === user && account.pin === pin
+    )
+    //
+    if (currentAccount) {
+      setAccount(currentAccount)
+    }
+  }
+
   return (
     <>
       <nav>
-        <Welcome user={user}/>
-        <Login />
+        <Welcome user={user} />
+        <Login onLogin={handleLogin} />
       </nav>
-      {user &&(
-      <main className="app">
-         <Balance movements={movements} />
-        <Movements movements={movements}/>
-        <Summary movements={movements} /> 
+      {user && (
+        <main className="app">
+          <Balance movements={movements} />
+          <Movements movements={movements} />
+          <Summary movements={movements} />
 
-        <div className="operation operation--transfer">
-          <h2>Transfer money</h2>
-          <form className="form form--transfer">
-            <input type="text" className="form__input form__input--to" />
-            <input type="number" className="form__input form__input--amount" />
-            <button className="form__btn form__btn--transfer">&rarr;</button>
-            <label className="form__label">Transfer to</label>
-            <label className="form__label">Amount</label>
-          </form>
-        </div>
+          <div className="operation operation--transfer">
+            <h2>Transfer money</h2>
+            <form className="form form--transfer">
+              <input type="text" className="form__input form__input--to" />
+              <input
+                type="number"
+                className="form__input form__input--amount"
+              />
+              <button className="form__btn form__btn--transfer">&rarr;</button>
+              <label className="form__label">Transfer to</label>
+              <label className="form__label">Amount</label>
+            </form>
+          </div>
 
-        <div className="operation operation--loan">
-          <h2>Request loan</h2>
-          <form className="form form--loan">
-            <input
-              type="number"
-              className="form__input form__input--loan-amount"
-            />
-            <button className="form__btn form__btn--loan">&rarr;</button>
-            <label className="form__label form__label--loan">Amount</label>
-          </form>
-        </div>
+          <div className="operation operation--loan">
+            <h2>Request loan</h2>
+            <form className="form form--loan">
+              <input
+                type="number"
+                className="form__input form__input--loan-amount"
+              />
+              <button className="form__btn form__btn--loan">&rarr;</button>
+              <label className="form__label form__label--loan">Amount</label>
+            </form>
+          </div>
 
-        <div className="operation operation--close">
-          <h2>Close account</h2>
-          <form className="form form--close">
-            <input type="text" className="form__input form__input--user" />
-            <input
-              type="password"
-              maxlength="6"
-              className="form__input form__input--pin"
-            />
-            <button className="form__btn form__btn--close">&rarr;</button>
-            <label className="form__label">Confirm user</label>
-            <label className="form__label">Confirm PIN</label>
-          </form>
-        </div>
+          <div className="operation operation--close">
+            <h2>Close account</h2>
+            <form className="form form--close">
+              <input type="text" className="form__input form__input--user" />
+              <input
+                type="password"
+                maxlength="6"
+                className="form__input form__input--pin"
+              />
+              <button className="form__btn form__btn--close">&rarr;</button>
+              <label className="form__label">Confirm user</label>
+              <label className="form__label">Confirm PIN</label>
+            </form>
+          </div>
 
-        <p className="logout-timer">
-          You will be logged out in <span className="timer">05:00</span>
-        </p>
-      </main>
+          <p className="logout-timer">
+            You will be logged out in <span className="timer">05:00</span>
+          </p>
+        </main>
       )}
     </>
   )
--- react-version/src/Movements/Movement.js
+++ react-version/src/Movements/Movement.js
@@ -15,10 +15,3 @@ function Movement({ position, amount }) {
 }
 
 export default Movement
-/* <div className="movements__row">
-        <div className="movements__type movements__type--withdrawal">
-          1 withdrawal
-        </div>
-        <div className="movements__date">24/01/2037</div>
-        <div className="movements__value">-378€</div>
-      </div> */
